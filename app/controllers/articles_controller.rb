5f9fbeb24272a5132686b2756c6074ad3b11497b
Deepeshac2890 <deepeshac290@gmail.com>
2023-08-16 11:23:59 +0530

    add Articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,11 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+        # return render json: {
+        #     success: true
+        # }
+    end
 end
